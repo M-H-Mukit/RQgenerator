9d4623dfb92d11d9ce05f7d92e1e8a99e9e7b4c1
Mukit <mehedi.mukit@gmail.com>
2017-06-16 00:41:55 +0600

    quote fetching solved

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //Declaring Variable
-var url      =  "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
+var url      =  "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?",
 	quote    =  $(".quote"),
 	author   =  $(".author"),
 	newQuote =  $("#myBut");
